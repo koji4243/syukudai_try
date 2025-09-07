b430d92392641a61ab702855dc0999623b836086
さくらの助 <koji.19@icloud.com>
2025-09-07 09:52:10 +0900

    宿題4

---
+++ syukudai_4/syukudai4.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// 問題：4
+// 2つの数値を引数に取り、合計を返す関数 addNumbers(a, b) を作成し、呼び出して結果をコンソールに表示してください
+
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+
+console.log(addNumbers(6,7));
